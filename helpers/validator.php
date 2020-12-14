8bddb704972a2f0463957114e8086f024fe1e518
JulitoM3 <l161080043@iztapalapa.tecnm.mx>
2020-12-14 17:06:09 -0600

    agregada funcion para validar URL

--- helpers/validator.php
+++ helpers/validator.php
@@ -5,13 +5,31 @@ class Validator
     private $errores = [];
     public function validar_vacio($campos)
     {
-        foreach ($campos as $campo => $valor) {
-            $camp = implode(' ', explode('_', $campo));
-            if (trim($valor) == '') {
+        if(is_array($campos)){
+
+            foreach ($campos as $campo => $valor) {
+                $camp = implode(' ', explode('_', $campo));
+                if (trim($valor) == '') {
+                    array_push($this->errores, 'El campo ' . $camp  . ' se encuentra vacío');
+                }
+            }
+        }else{
+            $camp = implode(' ', explode('_', $campos));
+            var_dump($campos);
+            if(trim($campos) == ''){
                 array_push($this->errores, 'El campo ' . $camp  . ' se encuentra vacío');
             }
         }
+        
     }
+ 
+
+    public function validar_url($URL){
+        if(!filter_var($URL, FILTER_VALIDATE_URL) & !preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|](\.)[a-z]{2}/i",$URL)){
+            array_push($this->errores, 'La url no es válida');
+        }
+    }
+
     public function validar_falla()
     {
         if (!count($this->errores) == 0) {
@@ -25,4 +43,6 @@ class Validator
     {
         return $this->errores;
     }
+
+
 }
