fa9b0edac19f8c5c42543818d635bc3b89827fef
JulitoM3 <l161080043@iztapalapa.tecnm.mx>
2020-12-14 17:05:42 -0600

    generando controlador para crear editoriales

---
+++ controladores/CreateEditorialsController.php
@@ -0,0 +1,24 @@
+<?php
+
+require('../modelos/Editoral.php');
+require('../helpers/validator.php');
+$editorial = new Editorial();
+$validator = new Validator();
+
+$varPOST = json_decode(file_get_contents('php://input'), true);
+//var_dump($varPOST);
+$validator->validar_vacio($varPOST);
+$validator->validar_url($varPOST['web_editorial']);
+ if(!$validator->validar_falla()){
+     http_response_code(200);
+     $nombre_editorial = $varPOST['nombre_editorial'];
+     $web_editorial = $varPOST['web_editorial'];
+
+     $editorial->createEditorial($nombre_editorial, $web_editorial);
+     
+}else{
+    http_response_code(500);
+    
+    echo json_encode($validator->get_errores(), JSON_PRETTY_PRINT);
+        
+ }
