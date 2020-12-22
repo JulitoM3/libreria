24152db534960a23c24f52e31fdd73c99ad6bf75
JulitoM3 <l161080043@iztapalapa.tecnm.mx>
2020-12-22 12:12:29 -0600

    quick fix a la carga asincrona de options de editoriales

--- assets/js/editoriales.js
+++ assets/js/editoriales.js
@@ -1,45 +1,80 @@
 const editorial_id = document.getElementById('editorial_id');
+const sav_book = document.getElementById('save_book');
+
+
 window.addEventListener('load', function () {
     axios({
         method: 'get',
         url: '../../controladores/ShowEditorialsController.php'
     })
         .then(function (response) {
-            console.log(response)
+           
+             let editoriales = response.data;
+             editoriales.forEach(editorial => {
+                 let new_editorial = document.createElement('option');
+                 new_editorial.textContent = editorial.nombre;
+                 new_editorial.id = editorial.id;
+                 editorial_id.appendChild(new_editorial);
+             })
+        })
+        .catch(function (errore) {
+            
+             let new_opt_edit = document.createElement('option');
+             new_opt_edit.textContent = errore.response.data;
+             new_opt_edit.id = 'default_editorial';
+             new_opt_edit.value = 'NA';
+             editorial_id.appendChild(new_opt_edit);
+
+             sav_book.setAttribute('disabled', 'true');
 
         })
-        .catch(function (error) {
-            let new_opt = document.createElement('option');
-            new_opt.textContent = error.response.data;
-            editorial_id.appendChild(new_opt);
-        })
+
 })
 
-const save = document.getElementById('save_editorial');
-const formulario = document.getElementById('frm-editorial');
-const ruta = formulario.getAttribute('action');
+const save_editorial = document.getElementById('save_editorial');
+const formulario_editorial = document.getElementById('frm-editorial');
+const ruta_editorial = formulario_editorial.getAttribute('action');
 const lista_editorial = document.getElementById('errors_editorial');
 let nombre_editorial = document.getElementById('nombre_editorial');
 let web_editorial = document.getElementById('web_editorial');
-save.addEventListener('click', function () {
+
+save_editorial.addEventListener('click', function () {
     axios({
         method: 'post',
-        url: ruta,
-        data:{
+        url: ruta_editorial,
+        data: {
             nombre_editorial: nombre_editorial.value,
             web_editorial: web_editorial.value
-        }
+        },
     })
         .then(function (response) {
-            
-        })
-        .catch(function (error) {
-            let errors = error.response.data 
+            console.log(response.data)
             while (lista_editorial.firstChild) {
                 lista_editorial.removeChild(lista_editorial.firstChild);
-              }
+            }
+
+            let new_i = document.createElement('li');
+            let new_option = document.createElement('option');
+            let default_editortial = document.getElementById('default_editorial')
+            if (typeof (default_editortial) != 'undefined' && default_editortial != null) {
+                default_editortial.remove();
+            }
+            new_option.textContent = nombre_editorial.value
+            new_option.value = response.data.id[0].fn_create_editoriales
+            new_i.textContent = 'Editorial añadido correctamente a la lista';
+            new_i.classList.add('alert');
+            new_i.classList.add('alert-success');
+            lista_editorial.appendChild(new_i);
+            editorial_id.appendChild(new_option)
+        })
+        .catch(function (errores) {
+            console.log(errores)
+            let errors = errores.response.data
+            while (lista_editorial.firstChild) {
+                lista_editorial.removeChild(lista_editorial.firstChild);
+            }
             errors.forEach(error => {
-                
+
                 let new_i = document.createElement('li');
                 new_i.textContent = error;
                 new_i.classList.add('alert');
--- controladores/ShowEditorialsController.php
+++ controladores/ShowEditorialsController.php
@@ -2,11 +2,12 @@
 require('../modelos/Editoral.php');
 $editorial = new Editorial();
 $editoriales = $editorial->getEditorials();
-
-if(is_object($editoriales)){
+ if (is_array($editoriales)) {
     http_response_code(200);
-    echo json_encode($editoriales,JSON_PRETTY_PRINT);
-}else{
+
+    echo json_encode($editoriales, JSON_PRETTY_PRINT);
+ } else {
     http_response_code(404);
-    echo json_encode($editoriales,JSON_PRETTY_PRINT);
-}
+
+     echo json_encode($editoriales, JSON_PRETTY_PRINT);
+ }
